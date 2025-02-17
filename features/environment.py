edbf53ac911ebd26a2af50d5703ccedf736cd1d9
thamodrensinniah <thamodren.sinniah@duerrdental.com>
2025-02-17 23:13:14 +0800

    demo fixture

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,7 @@ from behave import use_fixture, fixture
 from dotenv import load_dotenv
 
 from features.fixture.browser import ChromeBrowser
+from features.fixture.demo_fixture import DemoFixture
 
 
 load_dotenv()
@@ -11,6 +12,13 @@ def chrome_browser(context):
     context.driver = ChromeBrowser().get_chrome_driver()
     yield context.driver
     context.driver.quit()
+
+@fixture
+def demo_fixture(context):
+    context.demo_fixture = DemoFixture().message
+    yield context.demo_fixture
+    # Cleanup
+    context.demo_fixture = None
 
 def before_scenario(context, scenario):
     use_fixture(chrome_browser, context)
---
+++ features/fixture/demo_fixture.py
@@ -0,0 +1,3 @@
+class DemoFixture:
+    def __init__(self):
+        self.message: str = "Hello, I was called by a fixture"
